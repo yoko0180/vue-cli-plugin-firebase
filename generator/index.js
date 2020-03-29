435e0264d28922cc6ca576ce4829e78304626b68
yoko0180 <yoko0180@gmail.com>
2020-03-30 05:59:44 +0900

    extendPackage

--- generator/index.js
+++ generator/index.js
@@ -4,6 +4,22 @@ const fs = require('fs')
 module.exports = (api, _, __, invoking) => { // eslint-disable-line no-unused-vars
   api.render('./template')
 
+  api.extendPackage({
+    scripts: {
+      "deploy": "firebase deploy",
+      "deploy:functions": "firebase deploy --only functions",
+      "deploy:rule": "firebase deploy --only firestore:rules",
+      "deploy:hosting": "firebase deploy --only hosting",
+      "build:deploy": "npm run build && firebase deploy",
+      "build:deploy:hosting": "npm run build && firebase deploy --only hosting"
+    },
+    dependencies: {
+      "firebase": "latest",
+      "firebase-admin": "latest",
+      "firebase-functions": "latest"
+    },
+  })
+
   if (api.invoking) {
     if (!fs.existsSync(api.entryFile)) {
       throw Error('エントリーファイルがありません。')
