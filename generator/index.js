776180ade56f1201b98e7af3ff2740979fbd5859
yoko0180 <yoko0180@gmail.com>
2020-03-30 04:44:08 +0900

    api.invoking

--- generator/index.js
+++ generator/index.js
@@ -4,12 +4,12 @@ const fs = require('fs')
 module.exports = (api, _, __, invoking) => { // eslint-disable-line no-unused-vars
   api.render('./template')
 
-  api.onCreateComplete(async () => {
+  if (api.invoking) {
     if (!fs.existsSync(api.entryFile)) {
       throw Error('エントリーファイルがありません。')
     }
     api.injectImports(api.entryFile, `import './firebase'`)
-  })
+  }
   // api.render(files => {
   //   const insert_str = `import './firebase'`
   //   helper.insert_to_file(insert_str, 'src/main.js', files)
