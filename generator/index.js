7feae01c6aff5547a410b33f2c6bf20447317a37
yoko0180 <yoko0180@gmail.com>
2020-03-30 04:39:08 +0900

    onCreateComplete

--- generator/index.js
+++ generator/index.js
@@ -1,14 +1,15 @@
-const helper = require('./helper')
+// const helper = require('./helper')
 const fs = require('fs')
 
 module.exports = (api, _, __, invoking) => { // eslint-disable-line no-unused-vars
   api.render('./template')
 
-  if (!fs.existsSync(api.entryFile)) {
-    throw Error('エントリーファイルがありません。')
-  }
-  api.injectImports(api.entryFile, `import './firebase'`)
-
+  api.onCreateComplete(async () => {
+    if (!fs.existsSync(api.entryFile)) {
+      throw Error('エントリーファイルがありません。')
+    }
+    api.injectImports(api.entryFile, `import './firebase'`)
+  })
   // api.render(files => {
   //   const insert_str = `import './firebase'`
   //   helper.insert_to_file(insert_str, 'src/main.js', files)
